c09be8ca76868dd21b09ab634910c5c359f3b03a
Carlos Caro <carloscaro@Charlie-MacBook-Pro.local>
2019-12-13 11:29:57 -0500

    adding bora bora blade mobile

--- resources/views/pages/bora-bora.blade.php
+++ resources/views/pages/bora-bora.blade.php
@@ -4,40 +4,46 @@
     {{--  Banner Hero --}}
     <div class="home-banner text-center">
         <img id="home-banner-img-responsive" src="{{url('/images/bora-bora-banner-bg.png')}}" alt="" style="width: 100vw; height: 100vh;">
-        <div id="destinations-banner-blur-responsive-bora-bora">
+        <div id="destinations-banner-blur-responsive-bora-bora" class="d-none d-sm-block">
         </div>
         {{-- title --}}
         <div id="destinations-banner-text-responsive" style="">
-            <h1 class="text-uppercase font-size-70">
+            <h1 class="text-uppercase font-size-70 d-none d-sm-block">
                 bora bora<br>tahiti
             </h1>
-            <hr class="destinations-banner-title-separator">
+            <hr class="destinations-banner-title-separator d-none d-sm-block">
         </div>
         {{-- price --}}
         <div id="destination-banner-price-responsive">
-            <p class="destinations-price-banner">
+            <p class="destinations-price-banner d-none d-sm-block">
                 starting from $1200
             </p>
         </div>
         {{-- promo --}}
         <div id="destination-banner-promo-responsive">
-            <p class="destinations-promo-banner">
+            <p class="destinations-promo-banner d-none d-sm-block">
                 use promo code
             </p>
         </div>
         <div id="destination-banner-promo-responsive">
-            <p class="destinations-promo-banner font-size-70">
+            <p class="destinations-promo-banner font-size-70 d-none d-sm-block">
                 BB20
             </p>
         </div>
         {{-- tag line --}}
         <div id="destination-banner-tagline-responsive">
-            <p class="destinations-tagline-banner">
+            <p class="destinations-tagline-banner d-none d-sm-block">
                 Explore the city that never sleeps
             </p>
         </div>
         <div class="home-box">
             <p class="gray-home-sub-heading text-uppercase text-left">our destinations for 2020</p>
+        </div>
+        {{-- mobile --}}
+        <div id="" class="text-center d-block d-sm-none" style="position: absolute; top: 15vh; width: 100%;">
+            <h1 class="text-uppercase font-size-70">
+                bora bora<br>tahiti
+            </h1>
         </div>
     </div>
     {{--  Content  --}}
